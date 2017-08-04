4de3740a5a4e581ae9865f1a0b5846c465a60fc2
paulobh <paulo.amcampolina@gmail.com>
2017-08-04 20:30:13 -0300

    Add files via upload

---
+++ View/Interface.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace View
+{
+	partial class Interface
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.maskedTextCNPJ = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxDTAbertura = new System.Windows.Forms.TextBox();
+            this.textBoxRazaoSocial = new System.Windows.Forms.TextBox();
+            this.textBoxNomeFantasia = new System.Windows.Forms.TextBox();
+            this.textBoxAtividadePrincipal = new System.Windows.Forms.TextBox();
+            this.listBoxAtividadeSecundaria = new System.Windows.Forms.ListBox();
+            this.textBoxNaturezaJuridica = new System.Windows.Forms.TextBox();
+            this.textBoxLogradouro = new System.Windows.Forms.TextBox();
+            this.textBoxNumero = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // maskedTextCNPJ
+            // 
+            this.maskedTextCNPJ.Location = new System.Drawing.Point(23, 63);
+            this.maskedTextCNPJ.Mask = "00.000.000/0000-00";
+            this.maskedTextCNPJ.Name = "maskedTextCNPJ";
+            this.maskedTextCNPJ.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextCNPJ.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Consultar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDTAbertura
+            // 
+            this.textBoxDTAbertura.Location = new System.Drawing.Point(253, 63);
+            this.textBoxDTAbertura.Name = "textBoxDTAbertura";
+            this.textBoxDTAbertura.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDTAbertura.TabIndex = 2;
+            this.textBoxDTAbertura.Text = "Data  de Abertura";
+            // 
+            // textBoxRazaoSocial
+            // 
+            this.textBoxRazaoSocial.Location = new System.Drawing.Point(23, 98);
+            this.textBoxRazaoSocial.Name = "textBoxRazaoSocial";
+            this.textBoxRazaoSocial.Size = new System.Drawing.Size(330, 20);
+            this.textBoxRazaoSocial.TabIndex = 3;
+            this.textBoxRazaoSocial.Text = "Nome Empresarial";
+            // 
+            // textBoxNomeFantasia
+            // 
+            this.textBoxNomeFantasia.Location = new System.Drawing.Point(23, 133);
+            this.textBoxNomeFantasia.Name = "textBoxNomeFantasia";
+            this.textBoxNomeFantasia.Size = new System.Drawing.Size(330, 20);
+            this.textBoxNomeFantasia.TabIndex = 4;
+            this.textBoxNomeFantasia.Text = "Nome Fantasia";
+            // 
+            // textBoxAtividadePrincipal
+            // 
+            this.textBoxAtividadePrincipal.Location = new System.Drawing.Point(23, 213);
+            this.textBoxAtividadePrincipal.Name = "textBoxAtividadePrincipal";
+            this.textBoxAtividadePrincipal.Size = new System.Drawing.Size(330, 20);
+            this.textBoxAtividadePrincipal.TabIndex = 5;
+            this.textBoxAtividadePrincipal.Text = "Atividade Economica Principal";
+            // 
+            // listBoxAtividadeSecundaria
+            // 
+            this.listBoxAtividadeSecundaria.Enabled = false;
+            this.listBoxAtividadeSecundaria.FormattingEnabled = true;
+            this.listBoxAtividadeSecundaria.Location = new System.Drawing.Point(23, 251);
+            this.listBoxAtividadeSecundaria.Name = "listBoxAtividadeSecundaria";
+            this.listBoxAtividadeSecundaria.Size = new System.Drawing.Size(330, 82);
+            this.listBoxAtividadeSecundaria.TabIndex = 6;
+            this.listBoxAtividadeSecundaria.UseWaitCursor = true;
+            // 
+            // textBoxNaturezaJuridica
+            // 
+            this.textBoxNaturezaJuridica.Location = new System.Drawing.Point(23, 171);
+            this.textBoxNaturezaJuridica.Name = "textBoxNaturezaJuridica";
+            this.textBoxNaturezaJuridica.Size = new System.Drawing.Size(330, 20);
+            this.textBoxNaturezaJuridica.TabIndex = 7;
+            this.textBoxNaturezaJuridica.Text = "Natureza Juridica";
+            // 
+            // textBoxLogradouro
+            // 
+            this.textBoxLogradouro.Location = new System.Drawing.Point(359, 60);
+            this.textBoxLogradouro.Name = "textBoxLogradouro";
+            this.textBoxLogradouro.Size = new System.Drawing.Size(330, 20);
+            this.textBoxLogradouro.TabIndex = 8;
+            this.textBoxLogradouro.Text = "Logradouro";
+            // 
+            // textBoxNumero
+            // 
+            this.textBoxNumero.Location = new System.Drawing.Point(359, 98);
+            this.textBoxNumero.Name = "textBoxNumero";
+            this.textBoxNumero.Size = new System.Drawing.Size(106, 20);
+            this.textBoxNumero.TabIndex = 9;
+            this.textBoxNumero.Text = "Numero";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(482, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(207, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "Nome Empresarial";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(359, 133);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(106, 20);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "Nome Empresarial";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(482, 133);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(207, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "Nome Empresarial";
+            // 
+            // Interface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 529);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBoxNumero);
+            this.Controls.Add(this.textBoxLogradouro);
+            this.Controls.Add(this.textBoxNaturezaJuridica);
+            this.Controls.Add(this.listBoxAtividadeSecundaria);
+            this.Controls.Add(this.textBoxAtividadePrincipal);
+            this.Controls.Add(this.textBoxNomeFantasia);
+            this.Controls.Add(this.textBoxRazaoSocial);
+            this.Controls.Add(this.textBoxDTAbertura);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextCNPJ);
+            this.Name = "Interface";
+            this.Text = "Receita Federal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextCNPJ;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxDTAbertura;
+        private System.Windows.Forms.TextBox textBoxRazaoSocial;
+        private System.Windows.Forms.TextBox textBoxNomeFantasia;
+        private System.Windows.Forms.TextBox textBoxAtividadePrincipal;
+        private System.Windows.Forms.ListBox listBoxAtividadeSecundaria;
+        private System.Windows.Forms.TextBox textBoxNaturezaJuridica;
+        private System.Windows.Forms.TextBox textBoxLogradouro;
+        private System.Windows.Forms.TextBox textBoxNumero;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
